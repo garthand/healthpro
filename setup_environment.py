334b5d46ce75baa65c9a3c9c257f6f757f2d2f0b
garthand <garthand@github.com>
2022-04-06 00:34:46 -0500

    Updated pyenv install

--- setup_environment.py
+++ setup_environment.py
@@ -7,6 +7,20 @@ git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 sudo apt install make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+# Set up environment for pyenv
+# the sed invocation inserts the lines at the start of the file
+# after any initial comment lines
+sed -Ei -e '/^([^#]|$)/ {a \
+export PYENV_ROOT="$HOME/.pyenv"
+a \
+export PATH="$PYENV_ROOT/bin:$PATH"
+a \
+' -e ':a' -e '$!{n;ba};}' ~/.profile
+echo 'eval "$(pyenv init --path)"' >>~/.profile
+
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+# Reload shell
+exec $SHELL
 # Use pyenv to install Python 3.7.11
 pyenv install 3.7.11
 # Make Python 3.7.11 the default option when invoking python3
